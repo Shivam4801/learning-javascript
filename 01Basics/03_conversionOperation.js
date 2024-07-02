f5674373e5b4217213da53274c8e5d72755e19ba
Shivam4801 <116420259+Shivam4801@users.noreply.github.com>
2024-07-02 08:53:37 +0000

    Conversion of dataTypes

---
+++ 01Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "null" => 0
+// "undefined" => NaN
+// "true" => 1; false => 0
+
+let isLoggedIn = "33abc";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "shivam" => true
+// "null" => false
+// "undefined" => true
